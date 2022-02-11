96236c32a0d09419d13489e75f42a61bbdcce2bb
Abiola Doyinsola Adeyemi <58660602+Abidoyinsola1@users.noreply.github.com>
2022-02-11 13:37:04 +0100

    Interest component

---
+++ src/components/Interest.js
@@ -0,0 +1,10 @@
+const Interest = () => {
+    return (
+        <>
+        <h3>Interests</h3>
+        <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </>
+    )
+}
+
+export default Interest
